6c220d8175a513389dd8181d615013a0c39f2524
Ovidio Miranda <ovidiocbba@hotmail.com>
2024-12-26 18:01:06 -0400

    2.6 Refactoring Your Tests with test fixtures - Part 1

--- src/test/java/com/ovidiomirada/playwright/ASimplePlaywrightTest.java
+++ src/test/java/com/ovidiomirada/playwright/ASimplePlaywrightTest.java
@@ -1,35 +1,42 @@
 package com.ovidiomirada.playwright;
 
 import com.microsoft.playwright.Browser;
-import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class ASimplePlaywrightTest {
 
-  @Test
-  void shouldShowThePageTitle() {
-    Playwright playwright = Playwright.create();
-    Browser browser = playwright.chromium().launch();
-    Page page = browser.newPage();
+  Playwright playwright;
+  Browser browser;
+  Page page;
 
-    page.navigate("https://practicesoftwaretesting.com");
-    String title = page.title();
+  @BeforeEach
+  void setup() {
+    playwright = Playwright.create();
+    browser = playwright.chromium().launch();
+    page = browser.newPage();
+  }
 
-    Assertions.assertTrue(title.contains("Practice Software Testing"));
-
+  @AfterEach
+  void teardown() {
     browser.close();
     playwright.close();
   }
 
   @Test
-  void shouldShowSearchTermsInTheTitle() {
-    Playwright playwright = Playwright.create();
-    Browser browser = playwright.chromium().launch();
-    Page page = browser.newPage();
+  void shouldShowThePageTitle() {
+    page.navigate("https://practicesoftwaretesting.com");
+    String title = page.title();
 
+    Assertions.assertTrue(title.contains("Practice Software Testing"));
+  }
+
+  @Test
+  void shouldShowSearchTermsInTheTitle() {
     page.navigate("https://practicesoftwaretesting.com");
     page.locator("[placeholder=Search]").fill("Pliers");
     page.locator("button:has-text('Search')").click();
@@ -37,8 +44,5 @@ public class ASimplePlaywrightTest {
     int matchingProductCount = page.locator(".card").count();
 
     Assertions.assertTrue(matchingProductCount > 0);
-
-    browser.close();
-    playwright.close();
   }
 }
