3a9330ef5b7913e48adcac91b4903f3d50f79c39
Ovidio Miranda <ovidiocbba@hotmail.com>
2024-12-26 20:22:53 -0400

    3.10 Coding Exercise - Configuration Browser Options - Part 1

--- src/test/java/com/ovidiomirada/playwright/ASimplePlaywrightTest.java
+++ src/test/java/com/ovidiomirada/playwright/ASimplePlaywrightTest.java
@@ -1,15 +1,38 @@
 package com.ovidiomirada.playwright;
 
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Page;
-import com.microsoft.playwright.junit.UsePlaywright;
+import com.microsoft.playwright.Playwright;
+import java.util.Arrays;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-@UsePlaywright
+
 public class ASimplePlaywrightTest {
 
+  Playwright playwright;
+  Browser browser;
+  Page page;
+
+  @BeforeEach
+  public void setUp() {
+    playwright = Playwright.create();
+    browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false)
+        .setArgs(Arrays.asList("--no-sandbox", "--disable-extensions", "--disable-gpu")));
+    page = browser.newPage();
+  }
+
+  @AfterEach
+  public void tearDown() {
+    browser.close();
+    playwright.close();
+  }
+
   @Test
-  void shouldShowThePageTitle(Page page) {
+  void shouldShowThePageTitle() {
     page.navigate("https://practicesoftwaretesting.com");
     String title = page.title();
 
@@ -17,7 +40,7 @@ public class ASimplePlaywrightTest {
   }
 
   @Test
-  void shouldShowSearchTermsInTheTitle(Page page) {
+  void shouldShowSearchTermsInTheTitle() {
     page.navigate("https://practicesoftwaretesting.com");
     page.locator("[placeholder=Search]").fill("Pliers");
     page.locator("button:has-text('Search')").click();
