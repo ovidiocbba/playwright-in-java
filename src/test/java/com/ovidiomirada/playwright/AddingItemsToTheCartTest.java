dcc972c6578f3c9dff44dce8dc032ea9e6d423bd
Ovidio Miranda <ovidiocbba@hotmail.com>
2024-12-28 12:00:06 -0400

    4.23 Live Coding Demo - Using The UsePlaywright Annotation

--- src/test/java/com/ovidiomirada/playwright/AddingItemsToTheCartTest.java
+++ src/test/java/com/ovidiomirada/playwright/AddingItemsToTheCartTest.java
@@ -2,65 +2,39 @@ package com.ovidiomirada.playwright;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
-import com.microsoft.playwright.Browser;
-import com.microsoft.playwright.BrowserContext;
-import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
-import com.microsoft.playwright.Playwright;
-import com.microsoft.playwright.options.AriaRole;
-import java.util.Arrays;
+import com.microsoft.playwright.junit.UsePlaywright;
 import java.util.List;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+@UsePlaywright(HeadlessChromeOptions.class)
 public class AddingItemsToTheCartTest {
 
-  private static Playwright playwright;
-  private static Browser browser;
-  private static BrowserContext browserContext;
-
-  Page page;
-
-  @BeforeAll
-  public static void setUpBrowser() {
-    playwright = Playwright.create();
-    browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false)
-        .setArgs(Arrays.asList("--no-sandbox", "--disable-extensions", "--disable-gpu")));
-    browserContext = browser.newContext();
-  }
-
-  @BeforeEach
-  public void setUp() {
-    browserContext = browser.newContext(
-        new Browser.NewContextOptions().setViewportSize(1920, 1080));
-    page = browserContext.newPage();
-  }
-
-  @AfterAll
-  public static void tearDown() {
-    browser.close();
-    playwright.close();
-  }
-
+  @DisplayName("Search for pliers")
   @Test
-  void shouldShowSearchTermsInTheTitle() {
+  void searchForPliers(Page page) {
+    page.onConsoleMessage(msg -> System.out.println(msg.text()));
+
     page.navigate("https://practicesoftwaretesting.com");
     page.getByPlaceholder("Search").fill("Pliers");
     page.getByPlaceholder("Search").press("Enter");
-    page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Search")).click();
+
+    page.waitForLoadState();
+    page.waitForCondition(() -> page.getByTestId("product-name").count() > 0);
+
+    List<String> products = page.getByTestId("product-name").allTextContents();
+    Assertions.assertThat(products.get(0)).containsIgnoringCase("Pliers");
 
     assertThat(page.locator(".card")).hasCount(4);
 
-    List<String> productNames = page.locator("[data-test='product-name']").allTextContents();
+    List<String> productNames = page.getByTestId("product-name").allTextContents();
     Assertions.assertThat(productNames).allMatch(name -> name.contains("Pliers"));
 
     Locator outOfStockItem = page.locator(".card")
-        .filter(new Locator.FilterOptions().setHasText("Out of stock"))
-        .locator("[data-test='product-name']");
+        .filter(new Locator.FilterOptions().setHasText("Out of stock")).getByTestId("product-name");
 
     assertThat(outOfStockItem).hasCount(1);
     assertThat(outOfStockItem).hasText("Long Nose Pliers");
---
+++ src/test/java/com/ovidiomirada/playwright/HeadlessChromeOptions.java
@@ -0,0 +1,16 @@
+package com.ovidiomirada.playwright;
+
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.junit.Options;
+import com.microsoft.playwright.junit.OptionsFactory;
+import java.util.Arrays;
+
+public class HeadlessChromeOptions implements OptionsFactory {
+
+  @Override
+  public Options getOptions() {
+    return new Options().setLaunchOptions(new BrowserType.LaunchOptions().setArgs(
+        Arrays.asList("--no-sandbox", "--disable-extensions", "--disable-gpu")))
+        .setHeadless(false).setTestIdAttribute("data-test");
+  }
+}
