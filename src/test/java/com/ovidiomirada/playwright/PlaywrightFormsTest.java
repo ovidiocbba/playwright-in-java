949a8315cc2eff1be93352e61841bee9e608e185
Ovidio Miranda <ovidiocbba@hotmail.com>
2024-12-28 14:46:45 -0400

    5.27 Live Coding Demo - Testing For Mandatory Fields

--- src/test/java/com/ovidiomirada/playwright/PlaywrightFormsTest.java
+++ src/test/java/com/ovidiomirada/playwright/PlaywrightFormsTest.java
@@ -4,6 +4,7 @@ import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertTha
 
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.junit.UsePlaywright;
+import com.microsoft.playwright.options.AriaRole;
 import java.net.URISyntaxException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -11,6 +12,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 @UsePlaywright(HeadlessChromeOptions.class)
 public class PlaywrightFormsTest {
@@ -52,6 +55,35 @@ public class PlaywrightFormsTest {
 
       String uploadedFile = uploadField.inputValue();
       org.assertj.core.api.Assertions.assertThat(uploadedFile).endsWith("sample-data.txt");
+    }
+
+    @DisplayName("Mandatory fields")
+    @ParameterizedTest
+    @ValueSource(strings = {"First name", "Last name", "Email", "Message"})
+    void mandatoryFields(String fieldName, Page page) {
+      var firstNameField = page.getByLabel("First name");
+      var lastNameField = page.getByLabel("Last name");
+      var emailNameField = page.getByLabel("Email");
+      var messageField = page.getByLabel("Message");
+      var subjectField = page.getByLabel("Subject");
+      var sendButton = page.getByText("Send");
+
+      // Fill in the field values.
+      firstNameField.fill("Sarah-Jane");
+      lastNameField.fill("Smith");
+      emailNameField.fill("sarah-jane@example.com");
+      messageField.fill("Hello, world!");
+      subjectField.selectOption("Warranty");
+
+      // Clear one of the fields.
+      page.getByLabel(fieldName).clear();
+
+      sendButton.click();
+
+      // Check the error message for that field.
+      var errorMessage = page.getByRole(AriaRole.ALERT).getByText(fieldName + " is required");
+
+      assertThat(errorMessage).isVisible();
     }
   }
 }
