982004e9270a79b88b826ffae070c7de8ca3a89e
Ovidio Miranda <ovidiocbba@hotmail.com>
2024-12-31 17:32:43 -0400

    11.49 Live Coding With Page Objects

--- src/test/java/com/ovidiomirada/playwright/PlaywrightPageObjectTest.java
+++ src/test/java/com/ovidiomirada/playwright/PlaywrightPageObjectTest.java
@@ -152,6 +152,35 @@ public class PlaywrightPageObjectTest {
         Assertions.assertThat(item.total()).isEqualTo(item.quantity() * item.price());
       });
     }
+
+    @Test
+    void whenCheckingOutMultipleItems() {
+      // Add the first.
+      navBar.openHomePage();
+      productList.viewProductDetails("Bolt Cutters");
+      productDetails.increaseQuanityBy(2);
+      productDetails.addToCart();
+      // Add the second.
+      navBar.openHomePage();
+      productList.viewProductDetails("Slip Joint Pliers");
+      productDetails.addToCart();
+
+      navBar.openCart();
+
+      List<CartLineItem> lineItems = checkoutCart.getLineItems();
+
+      Assertions.assertThat(lineItems).hasSize(2);
+      List<String> productNames = lineItems.stream().map(CartLineItem::title).toList();
+      Assertions.assertThat(productNames).contains("Bolt Cutters", "Slip Joint Pliers");
+
+      Assertions.assertThat(lineItems).allSatisfy(item -> {
+        Assertions.assertThat(item.quantity()).isGreaterThanOrEqualTo(1);
+        Assertions.assertThat(item.price()).isGreaterThan(0.0);
+        Assertions.assertThat(item.total()).isGreaterThan(0.0);
+        Assertions.assertThat(item.total()).isEqualTo(item.quantity() * item.price());
+      });
+
+    }
   }
 
   class SearchComponent {
